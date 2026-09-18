35822cd091f347234d292c4917abb16fab5ab46e
kanekarvaishnavi184-lab <kanekarvaishnavi184@gmail.com>
2026-09-18 15:28:13 +0530

    calculate

---
+++ calculate.py
@@ -0,0 +1,6 @@
+a=float(input("enter first number: "))
+b=float(input("enter second number: "))
+addition = a + b
+subtraction = a - b
+print("addition=",addition)
+print("subtraction=",subtraction)
