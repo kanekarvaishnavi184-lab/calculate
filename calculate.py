7631623b7c551a7a86cb16f5f0d34be35b12ab60
vaishnavi <vaishnavikanekar477@gmail.com>
2026-09-18 16:04:32 +0530

    added the multiplication division code.

--- calculate.py
+++ calculate.py
@@ -2,5 +2,12 @@ a=float(input("enter first number: "))
 b=float(input("enter second number: "))
 addition = a + b
 subtraction = a - b
+multiplication = a * b
 print("addition=",addition)
 print("subtraction=",subtraction)
+print("multiplication=",multiplication)
+if b != 0:
+  division=a/b
+  print("division=",division)
+else:
+  print("division by zero is not possible")
